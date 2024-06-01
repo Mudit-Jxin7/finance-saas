5c512c93b9053fbf42af8f326bd7ced278f001f2
Mudit-Jain7 <mudit@wizcommerce.com>
2024-06-01 19:32:13 +0530

    fix : error on get api

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,10 +1,9 @@
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { HTTPException } from "hono/http-exception";
+import { eq } from "drizzle-orm";
 
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
-import { eq } from "drizzle-orm";
 
 const app = new Hono()
     .get("/", clerkMiddleware(), async (c) => {
@@ -12,9 +11,7 @@ const app = new Hono()
         const auth = getAuth(c);
 
         if (!auth?.userId) {
-            throw new HTTPException(401, {
-                res: c.json({ error: "Unauthorized" }, 401),
-            })
+            return c.json({ error: "Unauthorized" }, 401);
         }
 
         const data = await db
