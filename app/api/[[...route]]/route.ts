9eb0af9ba0f537ffdd9de7993b4b27740092660b
Mudit-Jain7 <mudit@wizcommerce.com>
2024-06-01 19:28:30 +0530

    feat: get request setup

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,33 +1,25 @@
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
-
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+
+import accounts from './accounts';
+import { HTTPException } from 'hono/http-exception';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app
-    .get('/hello',
-        clerkMiddleware(),
-        (c) => {
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        return err.getResponse();
+    }
 
-            const auth = getAuth(c)
+    return c.json({ error: 'Internal Server Error' }, 500);
+})
 
-            if (!auth?.userId) {
-                return c.json({
-                    message: 'You are not authenticated',
-                })
-            }
-
-            return c.json({
-                message: 'Hello Next.js!',
-                userId: auth.userId,
-            })
-        })
-
+const routes = app
+    .route('accounts', accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes;
